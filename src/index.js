3b2cc2007fdf79e96a8c5c75bab6a2aac24a5cf0
zembahk <77575523+zembahk@users.noreply.github.com>
2021-10-31 19:40:24 -0700

    1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId="qSMoPYerfAq5EbV5uzrxd6WdVjjpx6d7Bwf89Hwy" serverUrl="https://zbmeaegcn0vy.usemoralis.com:2053/server">
+      <App />
+    </MoralisProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
