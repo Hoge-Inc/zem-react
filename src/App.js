86f8f239f7f6bfffa0bc604ff9bde2d60e18abe3
zembahk <77575523+zembahk@users.noreply.github.com>
2021-11-09 20:43:01 -0800

    some boilerplate code
    58 vulnerabilities (16 moderate, 40 high, 2 critical)
    
    loads slow but works

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
-import { useMoralis } from "react-moralis";
+import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
+import { contractAbi, CONTRACT_ADDRESS } from "./abi.js";
+import React, { useState } from 'react';
+
+
+function Form() {
+
+  const [tokenAddress, setAddress] = useState('');
+  const [tokenAmount, setAmount] = useState(''); 
+  const { user } = useMoralis();
+  const address = user.get("ethAddress");
+  const { fetch } = useWeb3ExecuteFunction({
+      abi: contractAbi,
+      contractAddress: CONTRACT_ADDRESS,
+      functionName: "someContractFunciton", // edit this to the function
+    
+      params: {
+        account: address,
+        token: tokenAddress, // edit to the contract function required inputs
+        amount: tokenAmount
+      },
+    });
+    
+  function handleSubmit(e) {
+    e.preventDefault(); 
+    
+    console.log('You clicked submit.');
+    console.log(tokenAddress);
+    console.log(tokenAmount);
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p></p>
+      <label>Please specify:</label>
+      <input type="text" name="address" placeholder=" contract address" value={tokenAddress} onInput={e => setAddress(e.target.value)} />
+      <br></br>
+      <label>Please specify:</label>
+      <input type="number" name="amount" placeholder=" amount to send" value={tokenAmount} onInput={e => setAmount(e.target.value)} />
+      <br></br>
+      <button class="button" type="submit" onClick={() => fetch()} >Submit</button>
+    </form>
+  );
+}
+
 
 function App() {
-  const { authenticate, isAuthenticated, user, logout } = useMoralis();
-  if (!isAuthenticated) {
+  const { authenticate, isAuthenticated, user, logout, isWeb3Enabled, isWeb3EnableLoading } = useMoralis();
+  if (!isAuthenticated && !isWeb3EnableLoading) {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <div>
-          <button class="button authenticateButton" onClick={() => authenticate()}>Authenticate</button>
-        </div>
-        </p>
-      </header>
-    </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p></p>
+          <div>
+            <button class="button authenticateButton" enabled={isWeb3Enabled.toString()} onClick={() => authenticate()}>Authenticate</button>
+          </div>
+
+        </header>
+      </div>
+    );
+  } else if (isWeb3EnableLoading) {
+    return (
+      <div>
+        Loading Web3...
+      </div>
     );
   }
 
@@ -23,12 +74,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <div>
-            <button class="button logoutButton" onClick={() => logout()}>Log Out</button>
-            <h1>Welcome {user.get("username")}</h1>
-          </div>
-        </p>
+        <p></p>
+        <div>
+          <button class="button logoutButton" onClick={() => logout()}>Log Out</button><p></p>
+          <h2>Welcome user {user.get("username")}</h2>
+          <h4>logged in with {user.get("ethAddress")}</h4>
+        </div>
+
+        <Form />
+
 
       </header>
     </div>
